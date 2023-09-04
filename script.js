137602a7eddff50c60b232011235241611e9bafb
gersonmb67 <134552777+BatataDeCodigos@users.noreply.github.com>
2023-09-04 17:33:38 -0300

    btn

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
 $(document).ready(function(){ 
 
 //botão 
-$("btn-success").click(function){
-    $('.btn.danger').button('toggle').addClass('fat')
-
-    alert(Aprendemos bootstrap, jQuery e um pouco de javaScript)
-
+$("btn-tec").click(function){
     
 
 })
